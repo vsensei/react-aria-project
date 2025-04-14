c05517eaa39c88eeb8357c305469904a3474f5f7
nmyagistu <nikita.myagistu@gmail.com>
2025-04-14 08:02:11 +0300

    Add CustomForm component

---
+++ src/components/CustomForm.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { Form } from 'react-aria-components';
+import Button from './Button';
+import NumberField from './NumberField';
+import Slider from './Slider';
+import CustomText from './TextField';
+import BinIcon from '../icons/BinIcon';
+
+import styles from './CustomForm.module.css';
+
+const minValue = -100;
+const maxValue = 100;
+const defaultValue = 0;
+
+export default function CustomForm() {
+  const [name, setName] = useState('');
+  const [fileSize, setFileSize] = useState(defaultValue);
+
+  return (
+    <Form
+      className={styles.form}
+      onSubmit={(e) => {
+        e.preventDefault();
+
+        alert(`name: '${name}'\nfileSize: ${fileSize}`);
+      }}
+      onReset={() => {
+        setName('');
+        setFileSize(defaultValue);
+      }}
+    >
+      <div className={styles.textField}>
+        <CustomText label='Name' value={name} setValue={setName} />
+      </div>
+      <div className={styles.size}>
+        <div className={styles.number}>
+          <NumberField
+            label='Size (GB)'
+            minValue={minValue}
+            maxValue={maxValue}
+            defaultValue={fileSize}
+            setValue={setFileSize}
+          />
+        </div>
+        <div className={styles.slider}>
+          <Slider
+            defaultValue={0}
+            value={fileSize}
+            minValue={minValue}
+            maxValue={maxValue}
+            setValue={setFileSize}
+          />
+        </div>
+      </div>
+      <div className={styles.buttons}>
+        <Button
+          type='reset'
+          label='Clear'
+          icon={<BinIcon />}
+          isMedium
+          isOutlined
+        />
+        <Button type='submit' label='Submit' isMedium />
+      </div>
+    </Form>
+  );
+}
