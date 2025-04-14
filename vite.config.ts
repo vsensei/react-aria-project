3716f6b19785a2eedc95b5c72beca633e84cbad5
nmyagistu <nikita.myagistu@gmail.com>
2025-04-14 08:34:19 +0300

    Update the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://vsensei.github.io/react-aria-project/',
   build: {
     outDir: './build',
   },
